d0b474cce83c4deec0010071fa42f58dfda9c7a4
shivnaik5 <shivang.naik@gmail.com>
2021-02-14 18:10:10 -0500

    Update home page. Add new experience section and images.

--- pages/index.js
+++ pages/index.js
@@ -43,25 +43,55 @@ class Index extends React.Component {
                 />
               </Col>
             </Row>
-            <Row>
-              <Col md='4'>
-                <div className='technical-experience'>
-                  <img className='image' src='/images/mn2.png' />
-                  <h1>Front End</h1>
-                  <p>Building responsive websites</p>
+            <div className='technical-experience'>
+              <Row>
+                <div className='tagline'>
+                  <h1>Experience Matters</h1>
                 </div>
-              </Col>
-              <Col md='4'>
-              <div className='technical-experience'>
-                <h1>Back End</h1>
-              </div>
-              </Col>
-              <Col md='4'>
-              <div className='technical-experience'>
-                <h1>Databases</h1>
-              </div>
-              </Col>
-            </Row>
+                <div className='description'>
+                  <p>From web services to networks to game development, having experience in a dearth of technologies is important for developers in the rapidly evolving world we live in today</p>
+                </div>
+              </Row>
+              <Row>
+                <Col md='4'>
+                  <div className='developer-experience'>
+                    <img className='image developer-image' src='/images/front-end-lg.png' />
+                    <h4>Front End</h4>
+                    <p>Building responsive web applications with user experience in mind</p>
+                  </div>
+                </Col>
+                <Col md='4'>
+                <div className='developer-experience'>
+                  <img className='image developer-image' src='/images/backend-lg.png' /> 
+                  <h4>Back End</h4>
+                  <p>Developing scalable and reliable services and APIs</p>
+                </div>
+                </Col>
+                <Col md='4'>
+                <div className='developer-experience'>
+                  <img className='image developer-image' src='/images/database-lg.png' />
+                  <h4>Databases</h4>
+                  <p>Implementing relational and NoSQL databases that are performant and resilient</p>
+                </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={{ size: 4, offset: 2 }}>
+                  <div className='developer-experience'>
+                    <img className='image developer-image' src='/images/solutions-architect-lg.png' />
+                    <h4>Solutions Architect</h4>
+                    <p>Designing service-oriented solutions and distributed systems</p>
+                  </div>
+                </Col>
+                <Col md={{ size: 4, offset: 0 }}>
+                <div className='developer-experience image'>
+                  <img className='image developer-image' src='/images/generalist-lg.png' /> 
+                  <h4>Generalist Programmer</h4>
+                  <p>Knowlege of TCP/IP, Unity and Computer Vision and languages including Go, Rust and Python</p>
+                </div>
+                </Col>
+              </Row>
+            </div>
           </Container>
         </div>
       </BaseLayout>
